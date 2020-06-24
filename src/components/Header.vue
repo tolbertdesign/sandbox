42fcb9f7ec9a3105fa4a452dc7409eb2ec5ba493
Victor Tolbert <victor.tolbert@gmail.com>
2020-06-23 20:35:46 -0400

    wip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,22 +15,6 @@
         <a class="mr-5 hover:text-gray-900">Contact</a>
         <a class="mr-5 hover:text-gray-900">Login</a>
       </nav> -->
-      <button
-        class="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-200 border-0 rounded focus:outline-none hover:bg-gray-300 md:mt-0"
-      >
-        Get Started
-        <svg
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          class="w-4 h-4 ml-1"
-          viewBox="0 0 24 24"
-        >
-          <path d="M5 12h14M12 5l7 7-7 7" />
-        </svg>
-      </button>
     </div>
 
     <Teleport to="#footer">
